14ddad27ae9e3e6fead5de899469387175b19d04
Prateet Tiwari <prateettiwari29@gmail.com>
2025-11-05 23:24:16 +0530

    Implement user registration and login functionality with JWT authentication

--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(express.json());
+
 const PORT = process.env.PORT || 5001;
 
 app.get('/', (req, res) => {
   res.send('Hello, SmartBuy App Backend!');
 });
+
+app.use('/api/users', userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
